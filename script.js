f4e4856b14a1f7edab00ef983e5e87f761695f2a
unknown <SumitK88>
2023-01-10 13:53:04 +0545

    initial

---
+++ script.js
@@ -0,0 +1,5 @@
+const img1=document.querySelector(".panel1");
+const img2=document.querySelector(".panel2");
+const img3=document.querySelector(".panel3");
+const img4=document.querySelector(".panel4");
+const img5=document.querySelector(".panel5");
